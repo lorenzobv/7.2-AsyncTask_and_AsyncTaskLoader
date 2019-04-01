eb38d7c144d684bde68a9de65f9dd9dfca9d8532
loren <lawrencebonnickvilela905@gmail.com>
2019-04-01 21:41:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loren/whowroteit/BookLoader.java
@@ -0,0 +1,32 @@
+package com.example.loren.whowroteit;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String>{
+
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    public BookLoader(@NonNull Context context) {
+        super(context);
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
